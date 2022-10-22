8e0f2225ea68ea5630d607a777cec1358430d1d5
Rahdeg <walett95@gmail.com>
2022-10-22 15:39:53 +0100

    set Server

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors({origin:true}));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.status(200).json({ msg: "Hello World" });
+  });
+  
+  app.all("*", (req, res) => {
+    res.send({
+      status: false,
+      messsage: "Oops! you've hit an invalid route.",
+    });
+  });
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+  console.log(`Running on PORT ${PORT}`);
+});
